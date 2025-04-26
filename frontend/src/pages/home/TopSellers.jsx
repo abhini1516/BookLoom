8fd7796ef65d692929cbdaa17622ac9331f191b9
abhini1516 <abhini.s.amcec@gmail.com>
2025-04-26 21:22:27 +0530

    Updated Home and TopSellers, added Recommened component

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import BookCard from '../books/BookCard';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import { Pagination,Navigation } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-
+import 'swiper/css/navigation';
 const categories = ["Choose a genre", "Business", "Fiction", "Horror", "Adventure"];
 
 const TopSellers = () => {
@@ -16,25 +16,22 @@ const TopSellers = () => {
     useEffect(() => {
         fetch("books.json")
             .then(res => res.json())
-            .then((data) => setBooks(data))
-            .catch((error) => console.error("Failed to fetch books:", error));
+            .then((data) => setBooks(data));
     }, []);
 
     const filteredBooks = selectedCategory === "Choose a genre"
         ? books
-        : books.filter(book =>
-            book.category?.toLowerCase() === selectedCategory.toLowerCase()
-        );
+        : books.filter(book => book.category === selectedCategory.toLowerCase());
+    console.log(filteredBooks);
 
     return (
         <div className='py-10'>
-            <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
-
+            <h2 className="text-3l font-semibold mb-6">Top Sellers</h2>
+            {/* Category filtering */}
             <div className='mb-8 flex items-center'>
                 <select
                     name="Category"
                     id="Category"
-                    value={selectedCategory}
                     className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'
                     onChange={(e) => setSelectedCategory(e.target.value)}
                 >
@@ -44,28 +41,35 @@ const TopSellers = () => {
                 </select>
             </div>
 
+            {/* Swiper Component */}
             <Swiper
                 slidesPerView={1}
-                spaceBetween={10}
-                pagination={{ clickable: true }}
+                spaceBetween={30}
+                navigation={true}
+
+                
                 breakpoints={{
                     640: {
-                        slidesPerView: 2,
+                        slidesPerView: 1,
                         spaceBetween: 20,
                     },
                     768: {
-                        slidesPerView: 4,
+                        slidesPerView: 2,
                         spaceBetween: 40,
                     },
                     1024: {
-                        slidesPerView: 5,
+                        slidesPerView: 2,
+                        spaceBetween: 50,
+                    },
+                    1180: {
+                        slidesPerView: 3,
                         spaceBetween: 50,
                     },
                 }}
-                modules={[Pagination]}
+                modules={[Pagination,Navigation]}
                 className="mySwiper"
-            >
-                {filteredBooks.map((book, index) => (
+            >   { 
+                filteredBooks.length > 0 && filteredBooks.map((book, index) => (
                     <SwiperSlide key={index}>
                         <BookCard book={book} />
                     </SwiperSlide>
