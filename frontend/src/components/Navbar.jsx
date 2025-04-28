d6eced60746367edca4ade4b3d23207cf5d023ad
abhini1516 <abhini.s.amcec@gmail.com>
2025-04-28 21:50:24 +0530

    Completed redux installation and add to cart

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { GiShoppingCart } from "react-icons/gi";
 
 import avatarImg from "../assets/avatar.png"
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const navigation = [
   { name: "Dashboard", href: "/dashboard" },
@@ -20,7 +21,9 @@ const navigation = [
 // filepath: /c:/Users/ananya/bookstore/frontend/src/components/Navbar.jsx
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-  console.log(isDropdownOpen)
+  const cartItems = useSelector(state => state.cart.cartItems);
+  
+
   const currentUser = false;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
@@ -74,7 +77,12 @@ const Navbar = () => {
 
           <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex item center rounded-sm">
             <GiShoppingCart className="size-6" />
-            <span className="text-sm font-semibold sm:ml-1">0</span>
+            {
+              cartItems.length > 0 ? <span className="text-sm font-semibold sm:ml-1>{cartItems.length}</span>">
+                  {cartItems.length}
+                </span>
+                : <span className="text-sm font-semibold sm:ml-1">0</span>}
+            
           </Link>
         </div>
       </nav>
