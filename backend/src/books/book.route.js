c7996c6b2f0929eef9e3a1c2460764d0a7f984a6
abhini1516 <abhini.s.amcec@gmail.com>
2025-05-09 15:04:58 +0530

    admin

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Book = require('./book.model');
 const {postABook, getAllBooks,getSingleBook,updateBook,deleteBook} = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router = express.Router();
 
 //frontend => backend server => controller => book schema => database => send to the server => back to the frontend
@@ -9,15 +10,15 @@ const router = express.Router();
 //get = when get something back from db
 //put/patch = when edit or update something
 //delete = when delete something
-router.post("/create-book",postABook);
+router.post("/create-book",verifyAdminToken, postABook);
 //get all books
-router.get("/",getAllBooks);
+router.get("/", getAllBooks);
 // single book
-router.get("/:id",getSingleBook);
+router.get("/:id", getSingleBook);
 //update a book endpoint
-router.put("/edit/:id",updateBook);
+router.put("/edit/:id",verifyAdminToken, updateBook);
 //delete a book
-router.delete("/:id",deleteBook);
+router.delete("/:id",verifyAdminToken, deleteBook);
 
 module.exports = router;
 //http://localhost:5000/api/books/create-book
