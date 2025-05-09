01d65625271b2a5292301d888b38f842076373a8
Ananya-R2004 <ananyarajesh2112@gmail.com>
2025-05-09 10:53:19 +0530

    admin

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.post("/admin",async (req,res) => {
+    const {username,password} = req.body;
+    try {
+        const admin =await User.findOne({username});
+
+    } catch (error) {
+        console.error("Error creating admin:", error);
+        res.status(401).send({message:"Invaild credentials"});
+    }
+})
 module.exports=router;
