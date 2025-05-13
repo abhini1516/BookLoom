378c222ba98a3b705585a110a64c7b9e450faec0
abhini1516 <abhini.s.amcec@gmail.com>
2025-05-13 17:27:17 +0530

    bookcard getimgurl

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
-import { getImgUrl } from '../../utils/getImgUrl';
+import { getImgUrl } from '../../utils/getimgUrl';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../redux/features/cart/cartSlice';
